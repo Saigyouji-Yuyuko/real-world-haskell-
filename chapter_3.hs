09366c7c0d52c162695def603bd6adb5fe4f83c5
caoziliang <caoziliang@jd.com>
2019-08-14 20:40:03 +0800

    c3

--- chapter_3.hs
+++ chapter_3.hs
@@ -1,3 +1,6 @@
+--import Prelude(Eq,Show,Double,String,Int,Num)
+
+
 data BookInfo = Book Int String [String]
                 deriving(Show)
 data MagzineInfo =  Magzine Int String [String]
@@ -15,7 +18,7 @@ data BetterReview = BetterReview BookInfo CustomerID ReviewBody
 
 type BookRecord = (BookInfo,BookReview)
 
-data Bool = False | True
+-- data Bool = False | True
 
 type CardHolder = String
 
@@ -42,3 +45,37 @@ c = Catacean "123" "456"
 
 d = Furniture "345" "765"
 
+data Cartesian2D    = Cartesian2D Double Double
+                    deriving(Eq,Show)
+
+data Polar2D    = Polar2D Double Double
+                deriving(Eq,Show)
+
+data Roygbiv    = Red
+                | Orange
+                | Yellow
+                | Green
+                | Blue
+                | Indigo
+                | Voilet
+                  deriving(Eq,Show)
+
+type Vector = (Double,Double)
+
+data Shape  = Circle Vector Double
+            | Poly [Vector]
+              deriving(Show)
+
+myNot True = False
+myNot False = True              
+
+sumList (x:xs)  = x + sumList xs
+sumList []      = 0
+
+third(a,b,c) =c
+
+complicated(True,a,x:xs,5) = (a,xs)
+
+bookID      (Book id title authors) = id
+bookTitle   (Book id title authors) = title
+bookAuthors (Book id title authors) = authors
